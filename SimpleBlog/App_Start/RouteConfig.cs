4f008c369a2adc7414b1e012bfec979ab4aad0ca
Jack Keene <jackkeene3531@yahoo.com>
2013-11-19 17:53:24 -0600

    Added .gitignore files, basic routes and controllers and the admin area

---
+++ SimpleBlog/App_Start/RouteConfig.cs
@@ -0,0 +1,28 @@
+﻿using SimpleBlog.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace SimpleBlog
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            var namespaces = new[] {typeof(PostsController).Namespace};
+
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+           
+            //Login Page Route 
+            routes.MapRoute("Login", "login", new { controller = "Auth", action = "login"}, namespaces);
+            
+            //Home Page Route
+            routes.MapRoute("Home", "", new { Controller = "Posts", action = "Index" }, namespaces);
+
+
+        }
+    }
+}
