d4740869729358fed0a98565b03e6cd38b7b7ba0
Jack Keene <jackkeene3531@yahoo.com>
2013-11-21 23:19:44 -0600

    Imported Styles and Scripts Jquery and Bootstrap

---
+++ SimpleBlog/App_Start/BundleConfig.cs
@@ -0,0 +1,37 @@
+﻿using System.Web.Http;
+using System.Web.Optimization;
+using Microsoft.Ajax.Utilities;
+
+namespace SimpleBlog
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            //Backend Style Bundles
+            bundles.Add(new StyleBundle("~/admin/styles")
+                .Include("~/content/styles/bootstrap.css")
+                .Include("~/content/styles/admin.css"));
+
+            //FrontEnd Style Bundles
+            bundles.Add(new StyleBundle("~/styles")
+                .Include("~/content/styles/bootstrap.css")
+                .Include("~/content/styles/site.css"));
+
+            //Backend Scripts
+            bundles.Add(new ScriptBundle("~/admin/scripts")
+                .Include("~/scripts/jquery-2.0.2.js")
+                .Include("~/scripts/jquery.validate.js")
+                .Include("~/scripts/jquery.validate.unobtrusive.js")
+                .Include("~/scripts/bootstrap.js"));
+
+            //Frontend Scripts
+            bundles.Add(new ScriptBundle("~/scripts")
+                .Include("~/scripts/jquery-2.0.2.js")
+                .Include("~/scripts/jquery.validate.js")
+                .Include("~/scripts/jquery.validate.unobtrusive.js")
+                .Include("~/scripts/bootstrap.js"));
+
+        }
+    }
+}
